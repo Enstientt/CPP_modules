dddd1a3d646a702b3637404f90cacf04e7e73d5a
Zakariae Essadqui <zessadqu@e2r4p15.1337.ma>
2023-08-06 22:17:41 +0100

    ds

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -3,11 +3,11 @@
 int calculate(int operand1, int operand2, char op)
 {
     if (op == '+')
-        return (operand1 + operand2);
+        return (operand2 + operand1);
     else if (op == '-')
-        return (operand1 - operand2);
+        return (operand2 - operand1);
     else if (op == '*')
-        return (operand1 * operand2);
+        return (operand2 * operand1);
     else if (op == '/')
     {
         if (operand2 == 0)
@@ -25,8 +25,17 @@ std::stack<int> rpn( char *av)
 {
     std::stack<int> stack;
     int i = 0;
+    int operand1;
+    int operand2;
     while (av[i])
     {
+        while(av[i]==' ')
+            i++;
+        if (av[i] == '\0')
+        {
+            std::cout << "Invalid Input" << std::endl;
+            exit(1);
+        }
         if (isdigit(av[i]))
             stack.push(av[i] - '0');
         else if (av[i] == '+' || av[i] == '-' || av[i] == '*' || av[i] == '/' )
@@ -36,10 +45,16 @@ std::stack<int> rpn( char *av)
                 std::cout << "Invalid Input" << std::endl;
                 exit(1);
             }
-            int operand1 = stack.top();
+
+            operand1 = stack.top();
             stack.pop();
-            int operand2 = stack.top();
+            operand2 = stack.top();
             stack.pop();
+            if (av[i] == '/' && operand1== 0)
+            {
+                std::cout << "division on zero" << std::endl;
+                exit(1);
+            }
             stack.push(calculate(operand1, operand2, av[i]));
         }
         i++;
@@ -49,12 +64,18 @@ std::stack<int> rpn( char *av)
 
 int main(int ac , char **av)
 {
+    std::string ipt;
     if (ac != 2)
     {
         std::cout << "Usage: ./RPN [file]" << std::endl;
         return 1;
     }
     std::string input = av[1];
+    if (input.empty())
+    {
+        std::cout << "Invalid Input" << std::endl;
+        return 1;
+    }
     if (input.find_first_not_of("+-*/0123456789 ") != std::string::npos)
     {
         std::cout << "Invalid Input" << std::endl;
